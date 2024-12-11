a3f9e9042649907b9a2d18ff0dba1c9f430e7135
kskibun <costam@costam.pl>
2024-12-11 21:13:51 +0100

    views updated to new model

--- main/views.py
+++ main/views.py
@@ -37,13 +37,11 @@ class CurrencyRateView(generics.ListAPIView):
 class LatestExchangeRate(APIView):
     def get(self, request, currency1, currency2):
         try:
-            currency1_code = get_object_or_404(Currency, currency_code__iexact=currency1)
-            currency2_code = get_object_or_404(Currency, currency_code__iexact=currency2)
-            latest_exchange_rate = CurrencyRate.objects.filter(currency1_id=currency1_code.id, currency2_id=currency2_code.id).order_by('-date').first()
+            latest_exchange_rate = CurrencyRate.objects.filter(currency1__currency_code=currency1.upper(), currency2__currency_code=currency2.upper()).order_by('-date').first()
             if latest_exchange_rate:
                 return Response(CurrencyRateSerializer(latest_exchange_rate).data, status=status.HTTP_200_OK)
             else:
-                return Response(f'Could not find records for currencies {currency1_code} and {currency2_code} in local DB', status=status.HTTP_404_NOT_FOUND)
+                return Response(f'Could not find records for currencies {currency1} and {currency2} in local DB', status=status.HTTP_404_NOT_FOUND)
         except CurrencyRate.DoesNotExist:
             return Response({'Could not get results from DB'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
