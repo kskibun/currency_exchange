f16fcb8559de05e6eb901e7b8ead7d3ab3146e49
kskibun <costam@costam.pl>
2025-02-04 18:52:40 +0100

    routing and module changes

--- currency_ex_front/src/app/app-routing.module.ts
+++ currency_ex_front/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {CurrencyExchangeRateComponent} from "./currency-exchange-rate/currency-exchange-rate.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'exchange_rate', component: CurrencyExchangeRateComponent}
 ];
 
 @NgModule({
--- currency_ex_front/src/app/app.module.ts
+++ currency_ex_front/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CurrencyExchangeRateComponent } from './currency-exchange-rate/currency-exchange-rate.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { CurrencyInputComponent } from './currency-input/currency-input.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CurrencyExchangeRateComponent,
+    CurrencyInputComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
