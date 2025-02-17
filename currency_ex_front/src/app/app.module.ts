3d140384ea7123ca4827ed8b09515a34a4f2c628
kskibun <costam@costam.pl>
2025-02-17 12:26:42 +0100

    interface and app module update

--- currency_ex_front/src/app/app.module.ts
+++ currency_ex_front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {HttpClientModule} from "@angular/common/http";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {MatButtonToggleModule} from "@angular/material/button-toggle";
     MatFormFieldModule,
     MatSelectModule,
     HttpClientModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- currency_ex_front/src/app/interfaces/currency.ts
+++ currency_ex_front/src/app/interfaces/currency.ts
@@ -1,4 +1,4 @@
 export interface Currency {
   continent: string;
-  currencyCode: string;
+  currency_code: string;
 }
